ed0d7ede62200cea7135b6488508c79ee1c72249
Lucas Kauff <l.kauff@rubika-edu.com>
2019-01-26 17:15:48 +0100

    HUD version mieux

---
+++ BlueBerry Jam 2019/Assets/LucasStuff/HUDScripts/JointVisibility.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JointVisibility : MonoBehaviour
+{
+    public GameObject startMemory;
+    public GameObject endMemory;
+    Image myImage;
+    bool isVisible;
+
+    private void Start()
+    {
+        myImage = GetComponent<Image>();
+        myImage.enabled = !myImage.enabled;
+        isVisible = false;
+    }
+
+    private void Update()
+    {
+        if (startMemory.GetComponent<MemoryTrigger>().memory.discovered && endMemory.GetComponent<MemoryTrigger>().memory.discovered && !isVisible)
+        {
+            myImage.enabled = !myImage.enabled;
+            isVisible = true;
+        }
+    }
+}
